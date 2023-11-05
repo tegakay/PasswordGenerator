875e6242b43dcc79bf94a7caba7071a397dc2ffb
Oghenebrume Kofi <kofitega@gmail.com>
2023-11-05 18:21:53 +0100

    thes

--- password-gen/src/main.tsx
+++ password-gen/src/main.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
-// add the beginning of your app entry
-import 'vite/modulepreload-polyfill'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ChakraProvider>
--- password-gen/vite.config.ts
+++ password-gen/vite.config.ts
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    // generate manifest.json in outDir
-    manifest: true,
-    rollupOptions: {
-      // overwrite default .html entry
-      input: '/src/main.tsx',
-    },
-  },
+  plugins: [react()]
 })
