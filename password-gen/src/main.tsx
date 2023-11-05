4eab159af09d4f89003cc066fd6915f1c401d9da
Oghenebrume Kofi <kofitega@gmail.com>
2023-11-05 17:56:10 +0100

    fix

--- password-gen/src/main.tsx
+++ password-gen/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ChakraProvider } from '@chakra-ui/react'
+// add the beginning of your app entry
+import 'vite/modulepreload-polyfill'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <ChakraProvider>
